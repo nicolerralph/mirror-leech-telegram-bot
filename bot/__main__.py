0172f3448d8ad4353e0e4322a93ce1ac30b1ad4e
orlandokc <94543487+orlandokc@users.noreply.github.com>
2022-06-15 05:24:51 +0000

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -65,8 +65,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("Repo", "")
-    buttons.buildbutton("Report Group", "")
+    buttons.buildbutton("Repo", "t.me/nemesis_god")
+    buttons.buildbutton("Report Group", "t.me/nemesis_god")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
